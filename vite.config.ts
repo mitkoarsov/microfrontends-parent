0c4cfe9b14598c56e9bcdad4d5f14b1866ab6dc7
mitkoarsov <mitko.arsov.dev@gmail.com>
2025-10-26 16:54:54 +0100

    config: including .env file

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
+const isProd: boolean = process.env.REACT_APP_IS_PROD === "true";
+const app1Url: string = isProd
+  ? "https://mitkoarsov.github.io/microfrontends-child-one/assets/remoteEntry.js"
+  : "http://localhost:5174/assets/remoteEntry.js";
+const app2Url: string = isProd
+  ? "https://mitkoarsov.github.io/microfrontends-child-two/assets/remoteEntry.js"
+  : "http://localhost:5175/assets/remoteEntry.js";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,10 +18,12 @@ export default defineConfig({
       filename: "remoteEntry.js",
       exposes: { "./global": "./src/global-api.ts" },
       remotes: {
+        app1: app1Url,
+        app2: app2Url,
         // app1: "http://localhost:5174/assets/remoteEntry.js",
         // app2: "http://localhost:5175/assets/remoteEntry.js",
-        app1: "https://mitkoarsov.github.io/microfrontends-child-one/assets/remoteEntry.js",
-        app2: "https://mitkoarsov.github.io/microfrontends-child-two/assets/remoteEntry.js",
+        // app1: "https://mitkoarsov.github.io/microfrontends-child-one/assets/remoteEntry.js",
+        // app2: "https://mitkoarsov.github.io/microfrontends-child-two/assets/remoteEntry.js",
       },
       shared: [
         "react",
